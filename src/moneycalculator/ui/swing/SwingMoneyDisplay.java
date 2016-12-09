0286e20549bd8b92cf801df747187558df1d63ea
usuario <usuario@10.22.143.46>
2016-12-09 09:47:14 +0000

    Version 1 Otra clase

---
+++ src/moneycalculator/ui/swing/SwingMoneyDisplay.java
@@ -0,0 +1,14 @@
+package moneycalculator.ui.swing;
+
+import javax.swing.JPanel;
+import moneycalculator.model.Money;
+import moneycalculator.ui.MoneyDisplay;
+
+public class SwingMoneyDisplay extends JPanel implements MoneyDisplay{
+
+    @Override
+    public void display(Money money) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     
+}
